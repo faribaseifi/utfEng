24dca230dc1ef7c4b4abf4bb66adc588dfd2b268
Your Name <you@example.com>
2018-12-06 14:45:41 +0330

    new changessss yey!!!

--- src/screens/AboutUni.js
+++ src/screens/AboutUni.js
@@ -7,9 +7,62 @@ import { Link } from 'react-router-dom';
 class AboutUni extends Component {
 render(){
   return (
-    <div>
-      <h3>hello AboutUni</h3>
-    </div>
+    <div class="container">
+
+  	<div class="blog-page">
+  	<div class="row">
+
+
+  		<div class="margin-top-20 col-md-12">
+
+
+  			<div class="blog-post single-post">
+
+  				<img class="post-img" src="images/blog-post-02a.jpg" alt=""/>
+
+
+  				<div class="post-content">
+  					<h3>Bedroom Colors You'll Never Regret</h3>
+
+  					<ul class="post-meta">
+  						<li>Novemer 9, 2016</li>
+  						<li><a href="#">5 Comments</a></li>
+  					</ul>
+
+  					<p>Nam nisl lacus, dignissim ac tristique ut, scelerisque eu massa. Vestibulum ligula nunc, rutrum in malesuada vitae, tempus sed augue. Curabitur quis lectus quis augue dapibus facilisis. Vivamus tincidunt orci est, in vehicula nisi eleifend ut. Vestibulum sagittis varius orci vitae.</p>
+
+  					<div class="post-quote">
+  						<span class="icon"></span>
+  						<blockquote>
+  							Mauris aliquet ultricies ante, non faucibus ante gravida sed. Sed ultrices pellentesque purus, vulputate volutpat ipsum hendrerit sed neque sed sapien rutrum.
+  						</blockquote>
+  					</div>
+
+  					<p>In ut odio libero, at vulputate urna. Nulla tristique mi a massa convallis cursus. Nulla eu mi magna. Etiam suscipit commodo gravida. Cras suscipit, quam vitae adipiscing faucibus, risus nibh laoreet odio, a porttitor metus eros ut enim. Morbi augue velit, tempus mattis dignissim nec, porta sed risus. Donec eget magna eu lorem tristique pellentesque eget eu dui. Fusce lacinia tempor malesuada. Ut lacus sapien, placerat a ornare nec, elementum sit amet felis. Maecenas pretium lorem hendrerit eros sagittis fermentum.</p>
+  					<p>Phasellus enim magna, varius et commodo ut, ultricies vitae velit. Ut nulla tellus, eleifend euismod pellentesque vel, sagittis vel justo. In libero urna, venenatis sit amet ornare non, suscipit nec risus. Sed consequat justo non mauris pretium at tempor justo sodales. Quisque tincidunt laoreet malesuada. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vitae ante enim. Fusce sed elit est. Suspendisse sit amet mauris in quam pretium faucibus et aliquam odio. </p>
+
+
+
+
+  				</div>
+  			</div>
+
+  			<div class="margin-top-15"></div>
+
+
+  	</div>
+
+
+
+
+  	<div class="col-md-4">
+
+  	</div>
+  	</div>
+
+
+  </div>
+  </div>
   )
 }
 }
