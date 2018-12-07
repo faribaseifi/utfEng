57a5c122f51e296e1eee2ec8cb1d1dfced3359a6
Eddie CooRo <1300100eddiecooro@gmail.com>
2018-12-07 15:43:29 +0330

    Add infiniteScroll

--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -1,17 +1,39 @@
-
 import React, { Component } from 'react';
+import InfiniteScroller from 'react-infinite-scroller';
 import { Link } from 'react-router-dom';
 
-
-
 class ContactUs extends Component {
-render(){
-  return (
-    <div>
-      <h3>hello  ContactUs</h3>
-    </div>
-  )
-}
+  state = {
+    items: [],
+    hasMore: true
+  };
+
+  loadMore = pageNum => {
+    const postId = 1;
+    if (pageNum === 20) this.setState({ hasMore: false });
+    fetch(
+      `https://www.kanoonbook.ir/index.php/app/utfGetNews/${postId}?pageNum=${pageNum}&pageCount=5`
+    )
+      .then(res => res.json())
+      .then(data =>
+        this.setState(state => ({ items: state.items.concat(data) }))
+      )
+      .catch(err => console.error(err));
+  };
+
+  render() {
+    return (
+      <InfiniteScroller
+        pageStart={0}
+        loadMore={this.loadMore}
+        hasMore={this.state.hasMore}
+        loader={<div>LOADING>>>></div>}>
+        {this.state.items.map(item => (
+          <div>{JSON.stringify(item)}</div>
+        ))}
+      </InfiniteScroller>
+    );
+  }
 }
 
 export default ContactUs;
